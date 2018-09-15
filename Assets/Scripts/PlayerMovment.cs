3da863b2616ffb2d9ba21d2051e4921a6f2d6273
Toddolish <42237447+Toddolish@users.noreply.github.com>
2018-09-15 17:50:35 +1000

    update
    1.1 prototype phase

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -1,42 +1,83 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovment : MonoBehaviour
 {
-    [Header("Variables")]
+    [Header(" Base Variables")]
+    public LayerMask mask;
+    public float moveSpeed;
+    public float startMovementSpeed;
+    public float maxMoveSpeed;
+    public float speedMultiplier;
+    public float speedIncreaseRate;
+    private Vector3 moveDirection;
+    Rigidbody rigid;
+
+    [Header("Gliding")]
+    public bool soaring;
+    public float gravityScale;
+
+    [Header("Jump")]
+    public float jumpForce;
+    public bool IsGrounded;
+    public float hitDistance;
+    public bool jump;
     public float fallMultiplier = 2.5f;
     public float lowJumpMultiplier = 2f;
 
-    public float moveSpeed;
-    public float jumpForce;
-    public float gravityScale;
-    public bool soaring;
-    public bool jump;
-    public bool IsGrounded;
-    public float hitDistance;
-    public LayerMask mask;
+    [Header("Draft Boost")]
+    public bool boosting;
+    public float boostForce;
 
-    private Vector3 moveDirection;
-    Rigidbody rigid;
+    [Header("Energy Blast")]
+    //Activate Blast
+    public float energyBlastForce;
+    public bool energyBlast;
+    public float blastTimer;
+    //Blast cooldown
+    public float blastCooldownTimer;
+    public bool readyToBlast = true;
 
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
+        readyToBlast = true;
     }
     private void Update()
     {
-        UpdateStats();
         Jump();
         Soaring();
-        if (rigid.velocity.y < 0 && !soaring) // do not run this whilst soaring
+        if (rigid.velocity.y < 2 && !soaring && !IsGrounded) // do not run this whilst soaring
         {
-            rigid.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+            rigid.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier) * Time.deltaTime;
         }
+        UpdateStats();
     }
     private void FixedUpdate()
     {
+        EnergyBlast();
+        BoostingUp();
         Movement();
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Boost")
+        {
+            boosting = true;
+        }
+        if (other.gameObject.tag == "Void")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Boost")
+        {
+            boosting = false;
+        }
     }
     public void Jump()
     {
@@ -46,7 +87,7 @@ public class PlayerMovment : MonoBehaviour
         }
         //else if (rigid.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
         //{
-        //    rigid.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        //    rigid.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1f) * Time.deltaTime;
         //}
     }
     public void Movement()
@@ -55,13 +96,26 @@ public class PlayerMovment : MonoBehaviour
         float inputV = Input.GetAxis("Vertical") * Time.deltaTime;
 
         rigid.AddForce(new Vector3(inputH * moveSpeed, 0, inputV * moveSpeed), ForceMode.Impulse);
+        if(Input.GetKey(KeyCode.W))
+        {
+            moveSpeed += speedIncreaseRate;
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            moveSpeed = startMovementSpeed;
+        }
+
+        if (moveSpeed > maxMoveSpeed)
+        {
+            moveSpeed = maxMoveSpeed;
+        }
     }
     public void Soaring()
     {
-        if(Input.GetKey(KeyCode.Space) && !IsGrounded) //start soaring
+        if(Input.GetKey(KeyCode.Space) && jump) //start soaring
         {
             soaring = true;
-            Physics.gravity = new Vector3(0, -9f, 0);
+            Physics.gravity = new Vector3(0, -20f, 0);
         }
         else
         {
@@ -73,19 +127,55 @@ public class PlayerMovment : MonoBehaviour
     {
         if(IsGrounded)
         {
-            hitDistance = 0.35f;
+            hitDistance = 0.55f;
         }
         else
         {
-            hitDistance = 0.30f;
+            hitDistance = 0.5f;
         }
-        if(Physics.Raycast(transform.position - new Vector3(0,0.85f, 0), -transform.up, hitDistance, mask))
+        if(Physics.Raycast(transform.position, -transform.up, hitDistance, mask))
         {
             IsGrounded = true;
+            readyToBlast = true;
+            jump = false;
         }
         else
         {
+            jump = true;
             IsGrounded = false;
+        }
+    }
+    public void BoostingUp()
+    {
+        if(boosting)
+        {
+            rigid.AddForce(new Vector3(0, boostForce * Time.deltaTime, 0), ForceMode.Impulse);
+        }
+    }
+    public void Sliding()
+    {
+
+    }
+    public void EnergyBlast()
+    {
+        if (readyToBlast)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0) && !IsGrounded)
+            {
+                energyBlast = true;
+            }
+            if (energyBlast)
+            {
+                blastTimer += Time.deltaTime;
+                rigid.AddForce(new Vector3(0, 0, energyBlastForce * Time.deltaTime), ForceMode.Impulse);
+                if (blastTimer > 0.2)
+                {
+                    moveSpeed += 10; //increases movment speed when you use this skill
+                    energyBlast = false;
+                    blastTimer = 0;
+                    readyToBlast = false;
+                }
+            }
         }
     }
 }
