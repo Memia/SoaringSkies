f7a71b0ca4377c6fe175068f2f25a5f968b94b56
Memia <memia@hotmail.com.au>
2018-09-15 20:41:29 +1000

    Added ring
    Sonic style boost ring.

---
+++ Assets/Scripts/Ring.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ring : MonoBehaviour
+{
+    public GameObject player;
+    public float force = 5000f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            player.GetComponent<Rigidbody>().AddForce(0, 2000, force);
+        }
+    }
+}
